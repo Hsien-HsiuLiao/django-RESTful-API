8a34b4749a3e484c8e77b12d37d980826e520947
hsien <hsien_hsiu.liao@yahoo.com>
2024-09-03 11:04:09 -0700

    fix typo

--- demo/urls.py
+++ demo/urls.py
@@ -7,8 +7,8 @@ import store.views
 import store.api_views
 
 urlpatterns = [
-    path('api/v1/products', store.api_views.ProductList.api_view()),
-    
+    path('api/v1/products', store.api_views.ProductList.as_view()),
+
     path('admin/', admin.site.urls),
     path('products/<int:id>/', store.views.show, name='show-product'),
     path('cart/', store.views.cart, name='shopping-cart'),
