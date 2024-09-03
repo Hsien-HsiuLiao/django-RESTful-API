93a91d335d3512f1f05e1c774fe961e2e345a5b8
hsien <hsien_hsiu.liao@yahoo.com>
2024-09-03 08:23:46 -0700

    add Meta and method override

--- store/serializers.py
+++ store/serializers.py
@@ -2,5 +2,16 @@ from rest_framework import serializers
 
 from store.models import Product
 
-class ProductSerializer(serializers.ModelSerialier):
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        # fields we want to serialize
+        fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end')
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['is_on_sale'] = instance.is_on_sale()
+        data['current_price'] = instance.current_price()
+        return data
+
     
