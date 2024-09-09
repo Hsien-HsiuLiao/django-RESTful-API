3348eb8ec20cb61f3739c3c9f5aa65b07b2c4c0a
hsien <hsien_hsiu.liao@yahoo.com>
2024-09-09 14:24:13 -0700

    add ProductUpdateTestCase

--- store/tests.py
+++ store/tests.py
@@ -46,7 +46,25 @@ class ProductListTestCase(APITestCase):
     def test_list_products(self):
         products_count = Product.objects.count()
         response = self.client.get('/api/v1/products/')
+        # assert pagination fields exist
         self.assertIsNone(response.data['next'])
         self.assertIsNone(response.data['previous'])
+
         self.assertEqual(response.data['count'], products_count)
         self.assertEqual(len(response.data['results']), products_count)
+
+class ProductUpdateTestCase(APITestCase):
+    def test_update_product(self):
+        product = Product.objects.first()
+        response = self.client.patch(
+            '/api/v1/products/{}/'.format(product.id),
+            {
+                'name': 'New Product',
+                'description': 'Awesome product',
+                'price': 123.45,
+            },
+            format='json',
+        )
+        updated = Product.objects.get(id=product.id)
+        self.assertEqual(updated.name, 'New Product')
+
